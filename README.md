adab6c80ec22a62941d30597cad564fd427e81a9
guifrangolino <gsouza2651@gmail.com>
2023-02-27 18:48:22 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# lol-data-campeao
+ Página com os campeões do LOL, feita com HTML, CSS, JavaScript e a API que a Riot Games disponibiliza.
---
+++ script.js
@@ -0,0 +1,41 @@
+const campeaoContainer = document.querySelector('.campeoes')
+const campeao = document.querySelectorAll('.campeao')
+const campeaoImg = document.querySelectorAll('.campeao-img')
+const campeaoNome = document.querySelectorAll('.campeao-nome')
+const input = document.querySelector('.input-nome')
+const loadingGif = document.querySelector('.loading-gif')
+
+fetch('http://ddragon.leagueoflegends.com/cdn/13.4.1/data/pt_BR/champion.json')
+    .then(resp => resp.json())
+    .then(data => Object.keys(data.data))
+    .then(nomes => nomes.forEach(nome => {
+        if (nome == 'Fiddlesticks') nome = 'FiddleSticks'
+
+        const elem = `<li class="campeao">
+            <div class="img-container"><img class="campeao-img" src="http://ddragon.leagueoflegends.com/cdn/img/champion/centered/${nome}_0.jpg" alt="imagem do campeão"></div>
+            <p class="campeao-nome">${nome}</p>
+            </li>`
+            
+        campeaoContainer.insertAdjacentHTML('beforeend', elem)
+    }))
+    .catch(err => console.log(err))
+    .finally(loadingGif.style.display = 'none')
+
+
+input.addEventListener('input', inputListener)
+
+function inputListener() {
+    let champName = document.querySelectorAll('.campeao')
+    champName.forEach(champ => {
+        let nome = champ.children[1].innerHTML.toLowerCase()
+        
+        if (nome.includes(input.value.toLowerCase())) {
+            champ.style.display = 'flex'
+        } else {
+            champ.style.display = 'none'
+        }
+    })
+}
+
+// nome-invalido
+
